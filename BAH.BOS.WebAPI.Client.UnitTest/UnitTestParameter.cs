bacd65982cfae3760229cbf6acd8a0c5ff8131ee
Martin Wang <ronaltn@live.cn>
2016-05-28 23:55:52 +0800

    构建单元测试

---
+++ BAH.BOS.WebAPI.Client.UnitTest/UnitTestParameter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.Client.UnitTest
+{
+    /// <summary>
+    /// 单元测试过程中使用的参数。
+    /// </summary>
+    /// <remarks>
+    /// 所有参数均以演示数据中心为基准。
+    /// </remarks>
+    public class UnitTestParameter
+    {
+        public virtual string URL
+        {
+            //ipv4.fiddler地址是针对Fidder捕获本机http请求所设置的特殊地址，
+            //若没有使用Fidder捕获需求可替换为localhost。
+            get { return "http://ipv4.fiddler/K3Cloud"; }
+        }//end property
+
+        /// <summary>
+        /// 数据中心主键。
+        /// </summary>
+        public virtual string DBId
+        {
+            //主键获取通过查询管理中心T_BAS_DATACENTER_L表获得。
+            //SELECT * FROM T_BAS_DATACENTER_L
+            get { return "564dd7c5e6c525"; }
+        }//end property
+
+        /// <summary>
+        /// 用户名。
+        /// </summary>
+        public virtual string UserName
+        {
+            get { return "demo"; }
+        }//end property
+
+        /// <summary>
+        /// 用户密码。
+        /// </summary>
+        public virtual string Password
+        {
+            get { return "888888"; }
+        }//end property
+
+    }//end class
+}//end namespace
---
+++ net40/BAH.BOS.WebAPI.Client.UnitTest.ByKd/AuthServiceUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BAH.BOS.WebAPI.Client.UnitTest
+{
+    [TestClass]
+    public class AuthServiceUnitTest
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            var parameter = ParameterSingleton.GetInstance();
+            var result = APIClient.Login(parameter.URL)
+                                  .SetDBId(parameter.DBId)
+                                  .SetUserName(parameter.UserName)
+                                  .SetPassword(parameter.Password)
+                                  .ToKdAPIRequest()
+                                  .Execute<string>();
+        }//end method
+
+    }//end class
+}//end namespace
