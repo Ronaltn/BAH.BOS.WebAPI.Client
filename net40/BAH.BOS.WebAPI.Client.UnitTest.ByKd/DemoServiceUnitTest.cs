9b1238ba402ae12138c6ba37b51d9a99741db13f
Martin Wang <ronaltn@live.cn>
2016-06-30 11:13:46 +0800

    增加Demo的单元测试

---
+++ net40/BAH.BOS.WebAPI.Client.UnitTest.ByKd/DemoServiceUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BAH.BOS.WebAPI.Client.BusinessServiceOperation;
+
+namespace BAH.BOS.WebAPI.Client.UnitTest
+{
+    [TestClass]
+    public class DemoServiceUnitTest : BaseUnitTest<UnitTestParameter>
+    {
+        [TestMethod]
+        public virtual void TestDemoService()
+        {
+            this.TestHelloWorld();
+            this.TestHelloUser();
+        }
+
+        public virtual void TestHelloWorld()
+        {
+            var response = APIClient.CreateAPIOperation<ExecuteServiceOperation>(this.TestParameter.URL)
+                                    .SetClassName("BAH.BOS.WebAPI.ServiceStub")
+                                    .SetClassNameWithoutNamespace("Demo.DemoService")
+                                    .SetMethodName("HelloWorld")
+                                    .ToKdAPIRequest()
+                                    .ToAPIResponse<string>();
+        }
+
+        public virtual void TestHelloUser()
+        {
+            var response = APIClient.CreateAPIOperation<ExecuteServiceOperation>(this.TestParameter.URL)
+                                    .SetClassName("BAH.BOS.WebAPI.ServiceStub")
+                                    .SetClassNameWithoutNamespace("Demo.DemoService")
+                                    .SetMethodName("HelloUser")
+                                    .ToKdAPIRequest()
+                                    .ToAPIResponse<string>();
+        }
+
+    }//end class
+}//end namespace
