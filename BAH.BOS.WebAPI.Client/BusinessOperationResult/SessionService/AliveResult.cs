219759500aacffd917dae97b9c08b17c3aa8d23d
Martin Wang <ronaltn@live.cn>
2016-06-30 13:35:51 +0800

    封装Alive返回结果

---
+++ BAH.BOS.WebAPI.Client/BusinessOperationResult/SessionService/AliveResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.Client.BusinessOperationResult.SessionService
+{
+    /// <summary>
+    /// 检测登录会话是否有效的服务返回结果。
+    /// </summary>
+    public class AliveResult : ServiceResult<bool>
+    {
+
+    }//end class
+}//end namespace
