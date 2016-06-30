5a9ce7bee2532d03c881662b4264f65b356f8818
Martin Wang <ronaltn@live.cn>
2016-06-30 13:21:18 +0800

    添加Alive方法

--- BAH.BOS.WebAPI.Client/BusinessServiceOperation/SessionService.cs
+++ BAH.BOS.WebAPI.Client/BusinessServiceOperation/SessionService.cs
@@ -20,8 +20,19 @@ namespace BAH.BOS.WebAPI.Client.BusinessServiceOperation
         }//end constructor
 
         /// <summary>
+        /// 指定登录会话是否有效方法参数。
+        /// </summary>
+        /// <returns>返回实例本身。</returns>
+        public virtual SessionService Alive()
+        {
+            this.SetMethodName("Alive");
+            return this;
+        }//end method
+
+        /// <summary>
         /// 指定心跳方法参数。
         /// </summary>
+        /// <remarks>返回实例本身。</remarks>
         public virtual SessionService Heartbeat()
         {
             this.SetMethodName("Heartbeat");
